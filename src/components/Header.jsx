2548a47fb4e997f750d52d37e7058e10c655ef0b
Ilya Ibragimov <ilya.ibragimov@seznam.cz>
2025-06-19 22:55:13 +0200

    added styling for Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,15 +7,17 @@ const Header = () => {
         <header className='header'>
             <div className='header-wrapper'>
                 <div className='header-icon'>
-                    <img src={logo} alt="Logo"/>
+                    <a href='#'>
+                        <img src={logo} alt="Logo"/>
+                    </a>
                 </div>
                 <nav className='header-menu'>
                     <div className='header-menu-wrapper'>
-                        <a href='#experience'>Work Experience</a>
-                        <a href='#education'>Education</a>
-                        <a href='#skills'>Skills</a>
-                        <a href='#projects'>Projects</a>
-                        <a href='#contacts'>Contacts</a>
+                        <a className='header-menu-item' href='#experience'>Work Experience</a>
+                        <a className='header-menu-item' href='#education'>Education</a>
+                        <a className='header-menu-item' href='#skills'>Skills</a>
+                        <a className='header-menu-item' href='#projects'>Projects</a>
+                        <a className='header-menu-item' href='#contacts'>Contacts</a>
                     </div>
                 </nav>
                 <div className='burger' onClick={()=> toggleMobileMenu()}>
